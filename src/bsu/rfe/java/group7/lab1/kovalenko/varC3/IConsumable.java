13f52e228c616ee87fb037f2cbec06a536e1b611
kovalenkoko <kiril.kovalenko.79@outlook.com>
2021-10-16 17:55:56 +0300

    Added Consumable and Nutritious interfaces

---
+++ src/bsu/rfe/java/group7/lab1/kovalenko/varC3/IConsumable.java
@@ -0,0 +1,5 @@
+package bsu.rfe.java.group7.lab1.kovalenko.varC3;
+
+public interface IConsumable {
+    void consume();
+}
---
+++ src/bsu/rfe/java/group7/lab1/kovalenko/varC3/INutritious.java
@@ -0,0 +1,5 @@
+package bsu.rfe.java.group7.lab1.kovalenko.varC3;
+
+public interface INutritious {
+    int calculateCalories();
+}
