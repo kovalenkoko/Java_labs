54645cb72cb00f99c40be26743cdbe175b014660
kovalenkoko <kiril.kovalenko.79@outlook.com>
2022-03-06 18:38:39 +0300

    Initial commit, added MainFrame class

---
+++ src/bsu/rfe/java/group7/lab6/kovalenko/varB/MainFrame.java
@@ -0,0 +1,94 @@
+package bsu.rfe.java.group7.lab6.kovalenko.varB;
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+
+public class MainFrame extends JFrame {
+    // Константы, задающие размер окна приложения
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private JMenuItem pauseMenuItem;
+    private JMenuItem resumeMenuItem;
+    private JMenuItem effectMenuItem;
+    // Поле, по которому прыгают мячи
+    private Field field = new Field();
+    // Конструктор главного окна приложения
+    public MainFrame() {
+        super("Программирование и синхронизация потоков");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        // Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH)/2,
+                (kit.getScreenSize().height - HEIGHT)/2);
+        // Установить начальное состояние окна развѐрнутым на весь экран
+        setExtendedState(MAXIMIZED_BOTH);
+        // Создать меню
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu ballMenu = new JMenu("Мячи");
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            public void actionPerformed(ActionEvent event) {
+                String Id = JOptionPane.showInputDialog(MainFrame.this,"Введите идентификатор",
+                        "Присовение идентификатора",JOptionPane.QUESTION_MESSAGE);
+
+                field.addBall(Id);
+                if (!pauseMenuItem.isEnabled() &&
+                        !resumeMenuItem.isEnabled()) {
+                    // Ни один из пунктов меню не являются
+                    // доступными - сделать доступным "Паузу"
+                    pauseMenuItem.setEnabled(true);
+                    effectMenuItem.setEnabled(true);
+                }
+            }
+        };
+        menuBar.add(ballMenu);
+        ballMenu.add(addBallAction);
+        JMenu controlMenu = new JMenu("Управление");
+        menuBar.add(controlMenu);
+        Action pauseAction = new AbstractAction("Приостановить движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
+
+        JMenu effectMenu = new JMenu("Эффект");
+        Action doEffectAction = new AbstractAction("Мы-команда") {
+            public void actionPerformed(ActionEvent e) {
+                String str = JOptionPane.showInputDialog(MainFrame.this,"Введите идентификатор",
+                        "Эффект \"мы-команда\"",JOptionPane.QUESTION_MESSAGE);
+                if (str != null) field.pauseOfTheSame(str);
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        menuBar.add(effectMenu);
+        effectMenuItem = effectMenu.add(doEffectAction);
+        effectMenuItem.setEnabled(false);
+        // Добавить в центр граничной компоновки поле Field
+        getContentPane().add(field, BorderLayout.CENTER);
+    }
+    // Главный метод приложения
+    public static void main(String[] args) {
+        // Создать и сделать видимым главное окно приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
